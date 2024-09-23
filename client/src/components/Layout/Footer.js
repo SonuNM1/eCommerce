641757f62dc3f95aa55e1613e20703726341dd1c
Sonu N Mahto <sonu.mahto362000@gmail.com>
2024-09-23 15:12:43 +0530

    8

---
+++ client/src/components/Layout/Footer.js
@@ -0,0 +1,12 @@
+
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className='bg-dark text-light p-3' >
+        <h1 className='text-center' >All Rights Reserved &copy; Sonu NM</h1>
+    </div>
+  )
+}
+
+export default Footer
