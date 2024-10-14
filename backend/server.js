ce30deaf4cc5525f6effe9fdbc72d47a5cdc8a90
SonuNM1 <sonu.mahto362000@gmail.com>
2024-10-14 21:34:36 +0530

    build created

--- backend/server.js
+++ backend/server.js
@@ -28,13 +28,16 @@ app.use(express.json());
 app.use(morgan('dev'));
 
 // Get the __dirname equivalent for ES module
+
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 // Serve static files from the frontend build directory
+
 app.use(express.static(path.join(__dirname, '../../client/build')));
 
 // Routes
+
 app.use('/api/v1/auth', authRoutes);
 app.use('/api/v1/auth', userRoute);
 app.use('/api/v1/category', categoryRoutes);
